0b26ef12f546219dfa82fbef84f713405512c1e4
Shanie Leigh <yao.h.wang@uni.sydney.edu.au>
2020-07-05 19:30:29 +0800

    Initial Commit

---
+++ Shared/AVKitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AVKitTestApp.swift
+//  Shared
+//
+//  Created by Shane Leigh on 05/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct AVKitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
