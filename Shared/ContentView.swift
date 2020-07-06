163d95d3e7e3c4691bb4981d395e90d5f7a1aec0
Shanie Leigh <yao.h.wang@uni.sydney.edu.au>
2020-07-06 22:37:57 +0800

    add AVKit

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -6,10 +6,25 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct ContentView: View {
+    
+//    var videoURL = Bundle.main.url(forResource: "test", withExtension: "mp4")!
+    var videoURL = URL(string: "http://vfx.mtime.cn/Video/2019/03/19/mp4/190319212559089721.mp4")!
+    
     var body: some View {
-        Text("Hello, world!").padding()
+        VideoPlayer(player: AVPlayer(url: videoURL)) {
+            Text("Shane Leigh")
+                .font(.caption)
+                .foregroundColor(.white)
+                .background(Color.black.opacity(0.7))
+                .clipShape(Capsule())
+            
+            Spacer()
+        }
+        .edgesIgnoringSafeArea(.all)
+            
     }
 }
 
